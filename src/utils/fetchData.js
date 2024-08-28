6d90c03a2e5ce631aefb0638779e700696198685
mirhakamran <mirhakamran10@gmail.com>
2024-08-28 23:55:11 +0500

    first commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    },
-  };
-  
-  export const youtubeOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
-    },
-  };
-  
-  export const fetchData = async (url, options) => {
-    const res = await fetch(url, options);
-    const data = await res.json();
-  
-    return data;
-  };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '858bcdc9f6msh0cd54204d957073p1d46a6jsn32e9deba674d',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '858bcdc9f6msh0cd54204d957073p1d46a6jsn32e9deba674d',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
